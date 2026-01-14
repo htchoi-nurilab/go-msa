904ba36a57a1f22b6d2205d096ecad6ca9dd7b56
htchoi-nurilab <htchoi@nurilab.com>
2026-01-14 15:22:29 +0900

    feat: req dto validation 추가

--- user-service/internal/dto/user_dto.go
+++ user-service/internal/dto/user_dto.go
@@ -1,9 +1,9 @@
 package dto
 
 type UserCreateRequest struct {
-	Email    string `json:"email"`
-	Name     string `json:"name"`
-	Password string `json:"password"`
+	Email    string `json:"email" binding:"required,email"`
+	Name     string `json:"name" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type UserCreateResponse struct {
