6da695fe9e0a14b38523f59e6f4aae3c2f5658d2
htchoi-nurilab <htchoi@nurilab.com>
2026-01-15 15:11:48 +0900

    chore: 알림 서비스 gRPC 서버 실행 구성

--- notification-service/cmd/server/main.go
+++ notification-service/cmd/server/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"net"
+
 	"log"
 
-	"github.com/gin-gonic/gin"
 	"github.com/htchoi-nurilab/go-msa/notification-service/config/database"
+	"github.com/htchoi-nurilab/go-msa/notification-service/internal/domain"
+	"github.com/htchoi-nurilab/go-msa/notification-service/internal/grpcserver"
+	"github.com/htchoi-nurilab/go-msa/notification-service/internal/repository"
+	"github.com/htchoi-nurilab/go-msa/notification-service/internal/service"
+	"github.com/htchoi-nurilab/go-msa/notification-service/proto"
 	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
 )
 
 func main() {
@@ -14,8 +27,25 @@ func main() {
 	}
 
 	db := database.NewDatabase()
+	if err := db.DB().AutoMigrate(&domain.Notification{}); err != nil {
+		log.Fatal(err)
+	}
 
-	r := gin.Default()
+	notificationRepo := repository.NewNotificationRepository(db.DB())
+	notificationSvc := service.NewNotificationService(notificationRepo)
+	notificationServer := grpcserver.NewServer(notificationSvc)
 
-	r.Run(":8082")
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcSrv := grpc.NewServer()
+	notificationpb.RegisterNotificationServiceServer(grpcSrv, notificationServer)
+
+	log.Printf("notification gRPC server listening at %v", lis.Addr())
+	if err := grpcSrv.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
